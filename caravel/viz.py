6249c634d395cd124cb1f9eba61a01871a6fd3c3
George Ke <georgekeee@gmail.com>
2016-06-07 17:04:28 -0700

    Added hook for map style, huge performance boost from bounding box fix, added count text on clusters

--- caravel/viz.py
+++ caravel/viz.py
@@ -1680,8 +1680,8 @@ class MapboxViz(BaseViz):
     fieldsets = ({
         'label': None,
         'fields': (
-            'longitude',
-            'latitude',
+            ('longitude', 'latitude'),
+            'mapbox_style',
         )
     },)
 
@@ -1711,7 +1711,11 @@ class MapboxViz(BaseViz):
             for lon, lat in zip(df[fd.get('longitude')], df[fd.get('latitude')])
           ]
         }
-        return geo_json
+
+        return {
+            "geoJSON": geo_json,
+            "mapStyle": fd.get("mapbox_style")
+        }
 
 
 viz_types_list = [
