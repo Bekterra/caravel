8d4779cc3037a3d347863cd07feb6d3c908828ba
George Ke <georgekeee@gmail.com>
2016-06-17 18:08:47 -0700

    Custom colour for clusters/points + smart text colouring

--- caravel/viz.py
+++ caravel/viz.py
@@ -1703,6 +1703,7 @@ class MapboxViz(BaseViz):
         'fields': (
             'mapbox_style',
             'global_opacity',
+            'mapbox_color',
         )
     }, {
         'label': 'Viewport',
@@ -1832,6 +1833,7 @@ class MapboxViz(BaseViz):
             "viewportZoom": fd.get("viewport_zoom"),
             "renderWhileDragging": fd.get("render_while_dragging"),
             "tooltip": fd.get("rich_tooltip"),
+            "color": fd.get("mapbox_color"),
         }
 
 
